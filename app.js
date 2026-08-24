69eb57ec8cc4d7db33eef39a03e4e3cecaf0528e
Augusto Borges <Augusto.Borges@alunos.sc.senac.br>
2026-08-24 09:51:53 -0300

    Sistema de Login

---
+++ app.js
@@ -0,0 +1,14 @@
+const http=require('http')
+const url=require('url')
+
+var nome
+var cadUser
+var cadPassword
+
+var callback=function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','*')
+    var rota=url.parse(req.url,true)
+    var dados=url.parse(req.url,true).query
+
+    if(rota.pathname='/logi')
+}
