6109e119a0a871b197e1c58f6d5e6900236e14bc
Augusto Borges <Augusto.Borges@alunos.sc.senac.br>
2026-08-27 08:25:55 -0300

    Sistema de Login

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const http=require('http')
+/*const http=require('http')
 const url=require('url')
 
 var nome
 var cadUser
-var cadPassword
+var cadPassword             Ainda to fazendo
 
 var callback=function(req,res){
     res.setHeader('Access-Control-Allow-Origin','*')
     var rota=url.parse(req.url,true)
     var dados=url.parse(req.url,true).query
 
-    if(rota.pathname='/logi')
+    if(rota.pathname='/login')
 }
+    */
