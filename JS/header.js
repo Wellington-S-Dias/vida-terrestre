aa28098579ffd57c60740837e3dcbb7c5dc8f693
Wellington-S-Dias <wellington.dias@estudante.sc.senac.br>
2026-08-27 21:25:55 -0300

    Mudanças para começar uma nova Página de Pesquisa

---
+++ JS/header.js
@@ -0,0 +1,15 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 100) {
+
+        header.classList.add("active");
+
+    } else {
+
+        header.classList.remove("active");
+
+    }
+
+});
